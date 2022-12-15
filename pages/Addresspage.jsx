e1806a54b0d513ff098cd634fcebd377a6f2199f
ajaygarg786 <ajaygarglearner@gmail.com>
2022-12-15 19:48:52 +0530

    day_2

--- pages/Addresspage.jsx
+++ pages/Addresspage.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Address from '../components/Address';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 
 const Addresspage = () => {
   return (
